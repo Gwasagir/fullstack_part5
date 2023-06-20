0cc9be2f5c4f8d969de732506b81052a2e0da8e7
Niko Keurulainen <keurulainen.niko@gmail.com>
2023-06-20 21:24:35 +0300

    Done 5.7, not the prettiest tho

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -125,7 +125,7 @@ const App = () => {
     <br></br>
 
       {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+        <Blog key={blog.id} blog={blog}/>
       )}
 
     </div>
--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,43 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
 
+const Blog = ({blog}) => {
+  console.log('username' ,blog)
+  const [visible, setVisible] = useState(false)
+  const hideWhenVisible = { 
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    display: visible ? 'none' : '' 
+  }
+  const showWhenVisible = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    display: visible ? '' : 'none' 
+  }
+
+  const toggleDetails = () => {
+    setVisible(!visible)
+  }
+
+  return(
+    <div>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author}
+        <button onClick={toggleDetails}>view</button>
+      </div>  
+      <div style={showWhenVisible}>
+        {blog.title} {blog.author} 
+        <button onClick={toggleDetails}>hide</button> <br></br>
+        {blog.url} <br></br>
+        likes {blog.likes} <br></br>
+        {blog.user[0].name}
+      </div>
+  </div>
+  )
+}
 export default Blog
