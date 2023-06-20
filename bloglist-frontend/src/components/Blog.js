0bcc3bd4e11440e2ca068ebafb81e395f280351d
Niko Keurulainen <keurulainen.niko@gmail.com>
2023-06-20 21:26:57 +0300

    Small fix to 5.7

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 
 const Blog = ({blog}) => {
-  console.log('username' ,blog)
   const [visible, setVisible] = useState(false)
   const hideWhenVisible = { 
     paddingTop: 10,
